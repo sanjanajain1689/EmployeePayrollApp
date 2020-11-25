f89c7de0c8b7fd740ab8d21b754543089ad9f036
Sanjana Jain <sanjana.jain04@gmail.com>
2020-11-25 05:38:12 +0530

    Added: Populate Employee Payroll Object on submit

---
+++ js/empPayroll.js
@@ -0,0 +1,146 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.name-error');
+    name.addEventListener('input', function() {
+        if(name.value.length == 0) {
+            nameError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            nameError.textContent = "";
+        } catch (e) {
+            nameError.textContent = e;
+        }
+    });
+const salary = document.querySelector('#salary')
+const output = document.querySelector('.salary-output')
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+    });
+});
+class EmployeePayrollData{
+   //getters and setters
+    get id() {return this._id;}
+    set id(id){
+        this._id=id;
+    }
+    set name(name){
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$')
+        if(nameRegex.test(name)) this._name = name;
+        else throw "Name is Incorrect!";
+    }
+
+    get profilePic() {return this._profilePic;}
+    set profilePic(profilePic){
+        this._profilePic = profilePic;
+    }
+
+    get gender() {return this._gender;}
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get department() {return this._department;}
+    set department(department){
+        this._department = department;
+    }
+
+    get salary() {return this._salary;}
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get startDate() {return this._startDate;}
+    set startDate(startDate){
+        let newDate = new Date(startDate[2],startDate[1],startDate[0]);
+        if(newDate <= new Date()) this._startDate = newDate;
+        else throw 'Start Date is incorrect';
+    }
+
+    get notes() {return this._notes}
+    set notes(notes){
+        this._notes = notes;
+    }
+
+    //toString method
+    toString(){
+        return "id="+this.id+" : name="+this.name+
+                " : gender="+this.gender+" : Dept="+this.department+
+                " : salary="+this.salary+" : Start Date="+this.startDate
+                +" : Notes="+this.notes;
+    }
+}
+const save = () => {
+    try {
+        let employeePayroll = new EmployeePayrollData();
+        employeePayroll.name = document.getElementById('name').value;
+        employeePayroll.profilePic = getRadioValue(document.getElementsByName('profile'));
+        employeePayroll.gender = getRadioValue(document.getElementsByName('gender'));
+        employeePayroll.department = getCheckBoxValue(document.getElementsByClassName('checkbox'));
+        employeePayroll.salary = document.getElementById('salary').value;
+
+        let start = new Array();
+        start.push(document.getElementById('day').value);
+        start.push(document.getElementById('month').value);
+        start.push(document.getElementById('year').value);
+        employeePayroll.startDate = start;
+
+        employeePayroll.notes = document.getElementById('notes').value;
+        console.log(employeePayroll);
+        alert(employeePayroll);
+    }
+    catch (exception) {
+        console.error(exception); alert(exception);
+    }
+    employees.push(employeePayroll);
+}
+function createAndUpdateStorage(employeePayroll) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList != undefined)
+        employeePayrollList.push(employeePayroll);
+    else
+        employeePayrollList = [employeePayroll];
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+}
+
+function getRadioValue(radios) {
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            return radios[i].value;
+        }
+    }
+}
+function getCheckBoxValue(boxes) {
+    let boxlist = []
+    for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].checked) {
+            boxlist.push(boxes[i].value)
+        }
+    }
+    return boxlist;
+}
+const resetForm = () => {
+    setValue('#name', ' ');
+    unsetSelectedValue('[name=profile]');
+    unsetSelectedValue('[name=gender]');
+    unsetSelectedValue('[name=department]');
+    setValue('#salary', '');
+    setValue('#notes', '');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+}
+
+const unsetSelectedValue = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
