a3ad5783079ca7e974e1e6e165ef76cd7021c624
Sanjana Jain <sanjana.jain04@gmail.com>
2020-11-25 07:18:42 +0530

    Added-Creating Home Page

---
+++ js/empPayrollApp.js
@@ -0,0 +1,68 @@
+window.addEventListener('DOMContentLoaded',(event)=>{
+    createInnerHTML();
+})
+
+function createInnerHTML(){
+    const headerHTML=
+        "<th></th>"+
+        "<th>Emp Name</th>"+
+        "<th>Gender</th>"+
+        "<th>Department</th>"+
+        "<th>Salary</th>"+
+        "<th>Start Date</th>"+
+        "<th>Actions</th>";
+
+        let innerHTML = `${headerHTML}`;    
+        let empPayrollList = createEmployeePayrollJson();
+        for(const empData of empPayrollList) {
+            innerHTML = `${innerHTML}
+            <tr>
+                <td><img class="profile" src="${empData._profilePic}" alt="Profile Pic"></td>
+                <td>${empData._name}</td>
+                <td>${empData._gender}</td>
+                <td>${getDeptHTML(empData._department)}</td>
+                <td>RS ${empData._salary}</td>
+                <td>${empData._startDate}</td>
+                <td>
+                    <img id="${empData._id}" onclick="remove()" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                    <img id="${empData._id}" onclick="update()" alt="edit" src="../assets/icons/create-black-18dp.svg">
+                </td>
+            </tr>
+            `;
+        }
+        document.querySelector('#table-display').innerHTML = innerHTML;
+    }
+    
+    function getDeptHTML(deptList) {
+        let deptHTML = '';
+        for(const dept of deptList) {
+            deptHTML = `${deptHTML}
+            <div class="dept-label">${dept}</div>`;
+        }
+        return deptHTML;
+    }
+    function createEmployeePayrollJson(){
+        let empPayrollLocalList = [
+            {
+                _name:'Rohit Sarraf',
+                _gender:'Male',
+                _department:['Engineering','Finance'],
+                _salary:'500000',
+                _startDate:'29 Oct 2019',
+                _note:'',
+                _id: new Date().getTime(),
+                _profilePic:'../assets/profile-images/Ellipse-5.png'
+            },
+            {
+                _name:'Sanjana Jain',
+                _gender:'Female',
+                _department:['Sales'],
+                _salary:'400000',
+                _startDate:'18 Nov 2020',
+                _note:'',
+                _id: new Date().getTime()+1,
+                _profilePic:'../assets/profile-images/Ellipse-7.png'
+            }
+        ]
+        return empPayrollLocalList;
+    }
